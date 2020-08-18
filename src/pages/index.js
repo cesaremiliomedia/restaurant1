5a7199c74439dcd8f97a26c4d9371ad512cff171
Vanessa Mendez <vanessamendez@vanessas-air.attlocal.net>
2020-08-18 15:04:53 -0500

    adfasdfsaf

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,7 @@ import SlideShow from "../components/SlideShow"
 import ModalObject from "../components/ModalObject"
 import Video from "../components/video"
 import VisibilitySensor from "react-visibility-sensor"
+import "@reach/dialog/styles.css"
 
 import { Spring } from "react-spring/renderprops"
 import { ToastContainer } from "react-toastify"
