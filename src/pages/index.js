d1dd85efda8c547094f4fe8cc5a5741d4e6780ca
Vanessa Mendez <vanessamendez@vanessas-air.attlocal.net>
2020-08-18 12:14:38 -0500

    asdfasdfasf

--- src/pages/index.js
+++ src/pages/index.js
@@ -72,6 +72,7 @@ export default class Page extends Component {
                   }}
                   to={{
                     opacity: 1,
+                    minHeight: "280px",
                   }}
                 >
                   {props => (
