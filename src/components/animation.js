dc29bdc11934f4abcdd968c0556ff79147ac67e7
Cesar E. Castro <you@example.com>
2020-09-03 02:20:38 -0500

    asdf

--- src/components/animation.js
+++ src/components/animation.js
@@ -26,7 +26,7 @@ export default class LottieControl extends React.Component {
     }
 
     return (
-      <div style={{ height: "150px", position: "relative", zIndex: 1 }}>
+      <div style={{ height: "100%", position: "relative", zIndex: 1 }}>
         <Lottie
           options={defaultOptions}
           width={"100%"}
