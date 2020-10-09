10c3dd1bc09bae23764fd7787a7fc35ebb33b1a4
Cesar E. Castro <you@example.com>
2020-10-09 18:31:06 -0500

    asfdas

--- src/components/Restaurant/tacos.js
+++ src/components/Restaurant/tacos.js
@@ -131,7 +131,7 @@ export default function Tacos({ item }) {
       </p>
       <p
         style={{
-          marginTop: "-30px",
+          marginTop: "30px",
           fontSize: "10px",
           fontWeight: "600",
           color: "black",
